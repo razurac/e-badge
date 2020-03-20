0f7de97d6ab73fa1bb93952bf94f8a0f846237bd
Razurac <zelch.arno@gmail.com>
2020-03-20 17:47:38 +0100

    Added Argument Parser and preparedimage loader

--- src/main.py
+++ src/main.py
@@ -7,10 +7,26 @@ from PIL import Image,ImageDraw,ImageFont,ImageOps
 from distutils.util import strtobool
 import traceback
 import sys
+import argparse
+
+parser = argparse.ArgumentParser(description='Displays pictures on e-Paper')
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("-f", "--file", help="Input File for monochrome")
+group.add_argument("-p", "--prepared", action='store_true', help="Load prepared files")
+parser.add_argument("-fb", "--file_black", help="Load black File")
+parser.add_argument("-fr", "--file_red", help="Load black File")
+parser.add_argument("-t", "--threshold", required=False, type=int, default=125, help="Sets threshold for conversion")
+parser.add_argument("-b", "--bicolor", required=False, action='store_true', help="Enables Bicolor")
+parser.add_argument("-to", "--trsh_of", required=False, type=int, default=10, help="Changes threshold offset for bicolor conversion")
+parser.add_argument("-fl", "--flip", required=False, action='store_true', help="Flips color")
+args = vars(parser.parse_args())
+
+#if args["prepared"] and (args["file_black"] is None or args["file_black"]):
+#    parser.error("-p requires -fb and -fr")
 
 
 
-def convert_image(src_file, threshold = 125, bicolor = False, trsh_of = 40, flip = False):
+def convert_image(src_file=args["file"], threshold=args["threshold"], bicolor=args["bicolor"], trsh_of=args["trsh_of"]):
     image_file = Image.open(src_file)
     size = (epd4in2b.EPD_WIDTH, epd4in2b.EPD_HEIGHT)
     image_file = ImageOps.fit(image_file, size, Image.ANTIALIAS, centering=(0.0, 0.5))
@@ -21,17 +37,19 @@ def convert_image(src_file, threshold = 125, bicolor = False, trsh_of = 40, flip
         print("Bicolor is on")
         image_r = image_file.point(lambda p: p > threshold + trsh_of and 255)
         image_r = ImageOps.invert(image_r)
-        if flip:
-            return image_r, image_b
         return image_b, image_r
 
     return image_b
 
 
+def load_prepared_image(file_b=args["file_black"], file_r=args["file_red"]):
+    image_b = Image.open(file_b)
+    image_r = Image.open(file_r)
+    return image_b, image_r
 
 
 
-def push_image(pic, flip = False):
+def push_image(pic, flip=args["flip"]):
     blackimage = Image.new('1', (epd4in2b.EPD_WIDTH, epd4in2b.EPD_HEIGHT), 255)
     redimage = Image.new('1', (epd4in2b.EPD_WIDTH, epd4in2b.EPD_HEIGHT), 255)
 
@@ -56,6 +74,14 @@ def push_image(pic, flip = False):
         exit()
 
 
-picture = convert_image(sys.argv[1], int(sys.argv[2]), bool(strtobool(sys.argv[3])))
+def main():
+    if args["prepared"]:
+        picture = load_prepared_image()
+    else:
+        picture = convert_image()
 
-push_image(picture)
+    push_image(picture)
+
+
+if __name__ == "__main__":
+    main()
